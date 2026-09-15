42b08243a77fcf74e4398693906aa371d9ef5aee
Fija-khan <fijapathan9763@gmail.com>
2026-09-15 19:51:49 +0530

    [update]: enquiry admin and media URL configuration-rb-packers-movers

--- backend/enquiries/admin.py
+++ backend/enquiries/admin.py
@@ -1,10 +1,47 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Enquiry, EnquiryFile
 
 
 class EnquiryFileInline(admin.TabularInline):
     model = EnquiryFile
     extra = 0
+    readonly_fields = ("file_preview", "uploaded_at")
+
+    fields = (
+        "file",
+        "file_preview",
+        "uploaded_at",
+    )
+
+    def file_preview(self, obj):
+        if not obj.file:
+            return "-"
+
+        file_url = obj.file.url
+        file_name = obj.file.name.lower()
+
+        # Image preview
+        if file_name.endswith(
+            (".jpg", ".jpeg", ".png", ".gif", ".webp")
+        ):
+            return format_html(
+                '<a href="{}" target="_blank">'
+                '<img src="{}" width="120" height="90" '
+                'style="object-fit: cover; border-radius: 6px;" />'
+                "</a>",
+                file_url,
+                file_url,
+            )
+
+        # Video / other file
+        return format_html(
+            '<a href="{}" target="_blank">📁 View / Open File</a>',
+            file_url,
+        )
+
+    file_preview.short_description = "Preview"
 
 
 @admin.register(Enquiry)
